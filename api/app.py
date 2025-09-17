ea999de1cb128699fbe1dbaa1d8d0ea73f6f97e2
Omar Fdale <omarfdale@gmail.com>
2025-09-17 21:16:02 +0200

    Fix : import API + download modèle HuggingFace

--- api/app.py
+++ api/app.py
@@ -1,15 +1,41 @@
-from fastapi import FastAPI
+
+from fastapi import FastAPI, Request
 from pydantic import BaseModel
-from api.interface import multilabel_pipeline
+from interface import multilabel_pipeline
+from huggingface_hub import hf_hub_download
 
-model = multilabel_pipeline()
+import os
+import joblib
 
-app = FastAPI()
-
-class Item(BaseModel):
+# Classe de requête
+class Question(BaseModel):
     text: str
 
+# Création de l'application FastAPI
+app = FastAPI()
+
+# Téléchargement automatique du modèle
+if not os.path.exists("model"):
+    os.makedirs("model")
+
+MODEL_PATH = hf_hub_download(repo_id="omarfdale/stackoverflow-model",
+                             filename="model.pkl",
+                             cache_dir="model",
+                             local_dir="model",
+                             local_dir_use_symlinks=False)
+
+# Chargement du modèle et des classes
+model, mlb, vectorizer = joblib.load(MODEL_PATH)
+
+@app.get("/")
+def read_root():
+    return {"message": "Bienvenue sur l'API de suggestion de tags StackOverflow."}
+
 @app.post("/predict")
-def predict(item: Item):
-    predictions = model.predict([item.text])
-    return predictions[0]
+def predict_tags(question: Question):
+    try:
+        text = question.text
+        prediction = multilabel_pipeline(text, model, mlb, vectorizer)
+        return {"tags": prediction}
+    except Exception as e:
+        return {"error": str(e)}
