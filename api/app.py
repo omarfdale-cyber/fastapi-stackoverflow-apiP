2f622e98a0847a6a944b74804673702ba718d32a
Omar Fdale <omarfdale@gmail.com>
2025-09-17 22:05:00 +0200

    Fix: conversion .tolist() pour retour JSON

--- api/app.py
+++ api/app.py
@@ -1,27 +1,17 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from huggingface_hub import hf_hub_download
 import joblib
-import os
 
-# Téléchargement du modèle depuis Hugging Face
-MODEL_PATH = hf_hub_download(
-    repo_id="OmarFD/stackoverflow-tagger-model",
-    filename="model.pkl",
-    cache_dir="model",
-    local_dir="model"
-)
-
-# Chargement du modèle
-model = joblib.load(MODEL_PATH)
-
-# API FastAPI
 app = FastAPI()
 
-class InputData(BaseModel):
+model = joblib.load("model.pkl")
+
+class InputText(BaseModel):
     text: str
 
 @app.post("/predict")
-def predict(data: InputData):
-    prediction = model.predict([data.text])
-    return {"tags": prediction[0]}
+def predict_tags(input: InputText):
+    text = input.text
+    y_pred = model.predict([text])
+    predicted_tags = y_pred[0].tolist()
+    return {"predicted_tags": predicted_tags}
